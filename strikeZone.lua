84d89f1a0755aa24db6390b212b657c7d0d4eae9
ruenrback <runerback@hotmail.com>
2017-10-24 22:50:45 +0800

    17-10-24 patch 2
    lots of modification is coming

--- strikeZone.lua
+++ strikeZone.lua
@@ -1,3 +1,19 @@
+--[[
+	according to test, spawned target cannot hold mission objective, also cannot be teleported.
+	
+	marker should be activated in the begining, then 
+	move to target position, and show the mission objective.
+	
+	strike target must at ground level and with 0 meter high.
+	
+	so marker always exists but moves a lot. 
+	before call strike, spawn marker target, let marker move to marker target.
+	when marker is in position, recovery marker target, show the mission objective, 
+	spawn strike target, then call strike.
+	after strike, recovery strike target, hide mission objective.
+	
+	finally, recovery marker if condition allowed.
+--]]
 
 SCRIPT_NAME = "strikeZone"
 
@@ -28,63 +44,80 @@ onEDXInitialized = function()
             name = "",
             setName = "dangerMarkerSet",
             setID = -1,
-            height = 8000 --marker height
+            height = 100 --marker height
         }
-        data["strikeLength"] = 3600 --duration of one strike. should be test and update in code
+        data["strikeLength"] = 45000 --duration of one strike. should be test and update in code
 	else
         
 	end
 end
 
+function log(message)
+	OFP:displaySystemMessage(message)
+end
+
 function start()
-    EDX:serialTimer(strike, data["interval"])
+	log("start")
+    EDX:serialTimer("strike", data["interval"])
 end
 
 function updateStrikeCenter()
+	log('updateStrikeCenter')
     local radius = math.random(1, data["radius"])
     local dir = math.random(0, 359)
-    local dx = math.cos(math.rad(dir)) * radius
-    local dz = math.sin(math.rad(dir)) * radius
+    local rad = math.rad(dir)
+    local dx = math.cos(rad) * radius
+    local dz = math.sin(rad) * radius
 
     local center = data["center"]
-    local strikeCenter = data["strikeCnter"]
+    local strikeCenter = data["strikeCenter"]
     strikeCenter.x = center.x + dx
     strikeCenter.z = center.z - dz
 end
 
 function updateInterval()
-    data["interval"] = math.rad(16, 60) * 1000
+	log('updateInterval')
+    data["interval"] = math.rad(36, 60) * 1000
 end
 
 function strike(timerID)
+	log('strike')
     math.randomseed(os.time())
     updateStrikeCenter()
 
     local center = data["strikeCenter"]
     local marker = data["marker"]
-    marker.setID = OFP:spawnEntitySetAtPosition(marker.setName, center.x, marker.height, center.z)
-
+    --marker.setID = OFP:spawnEntitySetAtLocation(marker.setName, center.x, marker.height, center.z)
+	marker.setID = OFP:activateEntitySet(marker.setName)
     updateInterval()
-    EDX:setTimer(timerID, data["interval"])
+    --EDX:setTimer(timerID, data["interval"]) --strike only once for now
 end
 
 function onMarkerReady()
+	log('onMarkerReady')
+	--OFP:setObjectiveVisibility('objective', true)
     local args = data["params"]
-    OFP:callArtilleryStrike(args.army, data["marker"].name, args.ammoSize, args.ammoType, args.formation, args.delay)
+    local target = data["marker"].name
+    OFP:setInvulnerable(target, true)--for test
+    log('target - '..target)
+    OFP:callArtilleryStrike(args.army, target, args.ammoSize, args.ammoType, args.formation, args.delay)
     EDX:simpleTimer("onStrikeEnd", data["strikeLength"])
 end
 
 function onStrikeEnd(timerID)
+	log('onStrikeEnd')
     EDX:deleteTimer(timerID)
     stop()
 end
 
 function stop()
-    OFP:destroyEntitySet(data["marker"].setID)
+	log('stop')
+    --OFP:destroyEntitySet(data["marker"].setID)--for test
     data["marker"].setID = -1
 end
 
 function checkMarker(setName, setID, entities)
+	log('checkMarker')
     if data["marker"].setID == setID then
         data["marker"].name = entities[1]
         onMarkerReady()
